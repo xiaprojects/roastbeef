93bc8427f72c7b9b232f74457139cba2f1b371fe
Chris Morgan <chmorgan@gmail.com>
2025-02-07 12:43:27 -0500

    cli.sh - Set MY_UID and MY_GID before invoking docker, as required by docker-compose.yaml

--- cli.sh
+++ cli.sh
@@ -1,4 +1,4 @@
 # Script for getting a cli interface into docker
 
 echo "'cd data' to find the local files"
-docker compose run --rm cli
+MY_UID="$(id -u)" MY_GID="$(id -g)" docker compose run --rm cli
