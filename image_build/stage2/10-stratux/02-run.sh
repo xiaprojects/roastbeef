db67e4b48aa80257e8389eb66698c7433c9e7a39
Jim Jacobsen <jamez1970@gmail.com>
2025-02-26 21:29:22 -0600

    Added libjpeg62-turbo because OGN binary depends on it

--- image_build/stage2/10-stratux/02-run.sh
+++ image_build/stage2/10-stratux/02-run.sh
@@ -8,7 +8,9 @@ DEB_FILENAME_TMP=${ROOTFS_DIR}/tmp/${DEB_NAME}
 # copy dpkg into /tmp directory so its available in the chroot
 (cp ../../stratux/${DEB_NAME} ${DEB_FILENAME_TMP})
 on_chroot << EOF
-    apt install -y libncurses6
+    # needed for OGN for some reason
+    apt install --yes libjpeg62-turbo
+    apt install -y libncurses6 
     dpkg -i /tmp/${DEB_NAME}
 EOF
 
