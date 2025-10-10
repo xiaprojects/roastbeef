cef76861d20c1e927ad290f8bc54e62b5dbb71a0
xiaprojects <xiaprojects@users.noreply.github.com>
2025-10-10 22:55:55 +0200

    Enable ON-OFF of the HDMI Display #25

---
+++ image_build_rb/root/home/pi/on.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export XDG_RUNTIME_DIR=/tmp
+export XDG_SESSION_CLASS=user
+export XDG_SESSION_ID=5
+export XDG_SESSION_TYPE=wayland
+
+WAYLAND_DISPLAY=wayland-1 wlr-randr --output HDMI-A-1 --on
