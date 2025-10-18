d458394ef3d98b24ff3acc8802bc4422b4945c86
xiaprojects <xiaprojects@users.noreply.github.com>
2025-10-18 08:53:50 +0200

    Aircraft Settings #51

--- web/RB-01/js/main.js
+++ web/RB-01/js/main.js
@@ -1,3 +1,33 @@
+/**
+ * This file is part of RB.
+ *
+ * Copyright (C) 2024 XIAPROJECTS SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+ * This source is part of the project RB:
+ * 01 -> Display with Synthetic vision, Autopilot and ADSB
+ * 02 -> Display with SixPack
+ * 03 -> Display with Autopilot, ADSB, Radio, Flight Computer
+ * 04 -> Display with EMS: Engine monitoring system
+ * 05 -> Display with Stratux BLE Traffic
+ * 06 -> Display with Android 6.25" 7" 8" 10" 10.2"
+ *
+ * Community edition will be free for all builders and personal use as defined by the licensing model
+ * Dual licensing for commercial agreement is available
+ * Please join Discord community
+ *
+ */
 var app = angular.module('bobby', ['ui.router', 'mobile-angular-ui', 'appControllers']);
 
 var appControllers = angular.module('appControllers', []);
@@ -191,6 +221,11 @@ app.controller('MainCtrl', function ($scope, $http, $state) {
 	if (true) {
 		if (($scope.statusService === undefined) || ($scope.statusService === null)) {
 			$scope.statusService = new StatusService($scope, $http, $state);
+		}
+	}
+	if (true) {
+		if (($scope.aircraftService === undefined) || ($scope.aircraftService === null)) {
+			$scope.aircraftService = new AircraftService($scope, $http, $state);
 		}
 	}
 	// TODO: radio is optional, so we can manage this condition
