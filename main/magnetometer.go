2e8eb03ee3539f076438677394db8f83dd2831af
xiaprojects <github@xiaprojects.com>
2025-12-22 18:51:20 +0100

    Enabling Replay Log not working #83

--- main/magnetometer.go
+++ main/magnetometer.go
@@ -1,8 +1,31 @@
 /*
-	Copyright (c) 2024 XIAPROJECTS SRL
-	Distributable under the terms of The "BSD New" License
-	that can be found in the LICENSE file, herein included
-	as part of this header.
+	This file is part of RB.
+
+	Copyright (C) 2025 XIAPROJECTS SRL
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published
+	by the Free Software Foundation, version 3.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+	This source is part of the project RB:
+	01 -> Display with Synthetic vision, Autopilot and ADSB
+	02 -> Display with SixPack
+	03 -> Display with Autopilot, ADSB, Radio, Flight Computer
+	04 -> Display with EMS: Engine monitoring system
+	05 -> Display with Stratux BLE Traffic
+	06 -> Display with Android 6.25" 7" 8" 10" 10.2"
+
+	Community edition will be free for all builders and personal use as defined by the licensing model
+	Dual licensing for commercial agreement is available
+	Please join Discord community
 
 	magnetometer.go:
 	- Calibrate
@@ -11,6 +34,10 @@
 */
 
 package main
+
+import (
+	"encoding/json"
+)
 
 
 
@@ -25,4 +52,13 @@ type MagnetometerData struct {
 	Y			float64
 	Z			float64
 	Heading		int
+}
+
+
+func (p MagnetometerData) String() string {
+    data, err := json.Marshal(p)
+    if err != nil {
+        return "{}" // or handle error as needed
+    }
+    return string(data)
 }
